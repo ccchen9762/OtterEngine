e8cf85f68fc338b666d40b92de38ac4899ccb6e2
ccchen <age39303@gmail.com>
2023-08-03 23:37:48 -0700

    modify randomizer functions

--- OtterEngine/Common/Randomizer.cpp
+++
@@ -1,5 +0,0 @@
-#include "Randomizer.h"
-
-std::mt19937 Randomizer::s_random;
-std::uniform_int_distribution<uint32_t> Randomizer::s_distributionInt;
-std::uniform_real_distribution<double> Randomizer::s_distributionDouble;
--- OtterEngine/Common/Randomizer.h
+++ OtterEngine/Common/Randomizer.h
@@ -4,28 +4,36 @@
 
 class Randomizer {
 public:
-	static void Init() { 
-		s_random.seed(std::random_device()()); 
-	}
-	
-	static uint32_t GetInt() { 
-		return s_distributionInt(s_random);
-	}
-	
-	static uint32_t GetInt(uint32_t max, uint32_t min = 0) {
-		return min + (s_distributionInt(s_random) % (max - min + 1));
+	static void Init() {
+		s_random.seed(std::random_device{}());
 	}
 
-	static double GetFloat() {
-		return s_distributionDouble(s_random);
+	// int [0, INT_MAX]
+	static int GetInt() {
+		std::uniform_int_distribution<int> intDist;
+		return intDist(s_random);
 	}
 
-	static double GetFloat(uint32_t max, uint32_t min = 0) {
-		return min + (s_distributionDouble(s_random) * (max - min));
+	// int [min, max]
+	static int GetInt(int max, int min = 0) {
+		std::uniform_int_distribution<int> intDist(min, max);
+		return intDist(s_random);
+	}
+
+	// float [0.0, 1.0]
+	static float GetFloat() {
+		static std::uniform_real_distribution<float> distributionFloat;
+		return distributionFloat(s_random);
+	}
+
+	// float [min, max]
+	static float GetFloat(int max, int min = 0) {
+		static std::uniform_real_distribution<float> distributionFloat(min, max);
+		return distributionFloat(s_random);
 	}
 
 private:
 	static std::mt19937 s_random;
-	static std::uniform_int_distribution<uint32_t> s_distributionInt;
-	static std::uniform_real_distribution<double> s_distributionDouble;
 };
+
+std::mt19937 Randomizer::s_random;
